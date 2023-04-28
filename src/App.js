3916aa22cc4128baddc18737205b7aaa3423c8d2
Ryan Hubble <rhubble1987@gmail.com>
2023-04-28 13:19:54 -0400

    initial survivor stuff

--- src/App.js
+++ src/App.js
@@ -6,154 +6,80 @@ import {
   selector,
   useRecoilState,
   useRecoilValue,
+  useRecoilValueLoadable
 } from 'recoil';
-
-const traitCategories = [
-  {
-    id: 1,
-    name: 'Information',
-    allowed: true
-  },
-  {
-    id: 2,
-    name: "Second chance",
-    allowed: true
-  },
-  {
-    id: 3,
-    name: "Inflicts Exhaustion",
-    allowed: true
-  },
-  {
-    id: 4,
-    name: "Requires activation",
-    allowed: true
-  },
-  {
-    id: 5,
-    name: "Killer nerf",
-    allowed: true
-  }
-]
-
-const traits = [
-  {
-    id: 1,
-    name: "Grants Endurance",
-    allowed: true,
-    categories: [2]
-  },
-  {
-    id: 2,
-    name: "Inflicts Exhaustion",
-    allowed: true,
-    categories: [3]
-  },
-  {
-    id: 3,
-    name: "Activates on being unhooked",
-    allowed: true,
-    categories: [4]
-  },
-  {
-    id: 4,
-    name: "Reveals killer's aura",
-    allowed: true,
-    categories: [1]
-  },
-  {
-    id: 5,
-    name: "Reveals teammates' auras",
-    allowed: true,
-    categories: [1]
-  },
-  {
-    id: 6,
-    name: "Sabotages hooks",
-    allowed: true,
-    categories: [5]
-  },
-  {
-    id: 7,
-    name: "Reveals hook auras",
-    allowed: true,
-    categories: [1]
-  }
-]
-
-const perks = [
-  {
-    id: 1,
-    name: 'Dead Hard',
-    allowed: true,
-    traits: [1, 2, 3]
-  },
-  {
-    id: 2,
-    name: "Kindred",
-    allowed: true,
-    traits: [4,5]
-  },
-  {
-    id: 3,
-    name: "Lithe",
-    allowed: true,
-    traits: [2]
-  },
-  {
-    id: 4,
-    name: "Bond",
-    allowed: true,
-    traits: [5]
-  },
-  {
-    id: 5,
-    name: "Saboteur",
-    allowed: true,
-    traits: [6,7]
-  }
-]
-
-/* Logic for setting the exclusion of a category, trait, and perk.
-
-When a category's allowed status is changed, this change in state should always be reflected by 
-child traits and grandchild perks, and when a trait's allowed status changes this should 
-always be reflected by child perks.
-
-*/
+import { usePapaParse } from 'react-papaparse';
+import ToggleButton from 'react-bootstrap/ToggleButton'
+import Collapse from 'react-bootstrap/Collapse';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
-
-
-
+const survPerkUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR4W3mqgNZMO0dVt-8zx1H4R9I1tcvo6P66Im5U-QQTSmxhECopKzGbgxvlPdZRyUZGA-tiFihhgfne/pub?gid=435978149&single=true&output=csv'
+const survPerkTraitUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR4W3mqgNZMO0dVt-8zx1H4R9I1tcvo6P66Im5U-QQTSmxhECopKzGbgxvlPdZRyUZGA-tiFihhgfne/pub?gid=728899707&single=true&output=csv'
+const surivorUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR4W3mqgNZMO0dVt-8zx1H4R9I1tcvo6P66Im5U-QQTSmxhECopKzGbgxvlPdZRyUZGA-tiFihhgfne/pub?gid=2090851778&single=true&output=csv'
 function App() {
+  const [perks, setPerks] = useState([]);
+  const [traits, setTraits] = useState([]);
+  const [survivors, setSurvivors] = useState([]);
+  const { readRemoteFile } = usePapaParse();
 
-  const [allowedPerks, setAllowedPerks] = useState(perks)
-  const [allowedTraits, setAllowedTraits] = useState(traits)
-  const [allowedTraitCategories, setAllowedTraitCategories] = useState(traitCategories)
+  useEffect(() => {
+      readRemoteFile(survPerkUrl, {
+        header: true,
+        complete: (results) => {
+          setPerks (results.data)
+        }
+      });
+      readRemoteFile(survPerkTraitUrl, {
+        header: true,
+        complete: (results) => {
+          setTraits (results.data)
+        }
+      });
+      readRemoteFile(surivorUrl, {
+        header: true,
+        complete: (results) => {
+          setSurvivors(results.data)
+        }
+      })
+  }, []);
 
-  function figureOutTraitState(trait, allowedPerks, setAllowedTraits) {
-    const traitPerks = allowedPerks.filter((perk) => {
-      perk.traits.includes(trait.id)
-    })
 
-  }
+  const perksState = atom({
+    key: 'perksState',
+    default: perks
+  });
 
-  function figureOutPerkCategoryState() {
+  const traitsState = atom({
+    key: 'traitsState',
+    default: traits
+  });
 
-  }
+  const survivorsState = atom({
+    key: 'survivorsState',
+    default: survivors
+  });
 
-  function Perk({perk, allowedPerks, setAllowedPerks}) {
-    const [allowed, setAllowed] = useState(perk.allowed)
+  function Perk({perk}) {
+    const [perks, setPerks] = useRecoilState(perksState)
+    let perkStateToUpdate = perks.find(p => p.id === perk.id)
+    let allowed = perkStateToUpdate.allowed
+    const newState = !allowed
+
 
     function handleClickEvent() {
-      const newState = !allowed
-      let perkToUpdate = allowedPerks.find(p => p.id === perk.id)
-
-      perkToUpdate.allowed = newState
-      setAllowed(newState)
-      setAllowedPerks(allowedPerks)
-      setAllowedTraits()
+      const updatedPerkState = {
+        ...perkStateToUpdate,
+        allowed: newState
+      };
+      setPerks(currentState => {
+        const updatedPerks = currentState.map(p => (p.id === perk.id ? updatedPerkState : p));
+        return updatedPerks;
+      });
     }
 
     return (
@@ -163,15 +89,246 @@ function App() {
       </div>
     )
   }
-  
+
+  function Trait({trait}) {
+    const [traits, setTraits] = useRecoilState(traitsState)
+    let traitStateToUpdate = traits.find(t => t.id === trait.id)
+    let allowed = traitStateToUpdate.allowed
+    const newState = !allowed
+
+
+    function handleClickEvent() {
+      const updatedTraitState = {
+        ...traitStateToUpdate,
+        allowed: newState
+      };
+      setTraits(currentState => {
+        const updatedTraits = currentState.map(t => (t.id === trait.id ? updatedTraitState : t));
+        return updatedTraits;
+      });
+    }
+
+    return (
+      <div>
+        <input type="checkbox" id={trait.id} checked={allowed} onChange={handleClickEvent}/>
+        <label htmlFor={trait.id}>{trait.description}</label>
+      </div>
+    )
+  }
+
+  function Survivor({survivor}) {
+    const [survivors  , setSurvivors] = useRecoilState(survivorsState)
+    let survivorStateToUpdate = survivors.find(s => s.id === survivor.id)
+    let allowed = survivorStateToUpdate.allowed
+    const newState = !allowed
+
+
+    function handleClickEvent() {
+      const updatedSurvivorState = {
+        ...survivorStateToUpdate,
+        allowed: newState
+      };
+      setSurvivors(currentState => {
+        const updatedSurvivors = currentState.map(s => (s.id === survivor.id ? updatedSurvivorState : s));
+        return updatedSurvivors;
+      });
+    }
+
+    return (
+      <div>
+        <input type="checkbox" id={survivor.id} checked={allowed} onChange={handleClickEvent}/>
+        <label htmlFor={survivor.id}>{survivor.name}</label>
+      </div>
+    )
+
+
+  }
+
+  function BuildGenerator() {
+    const [chosenPerks, setChosenPerks] = useState([]);
+    const allPerks = useRecoilValue(perksState);
+    const currentAllowedPerks = allPerks.filter(p => p.allowed);
+
+    function handleClickEvent() {
+      setChosenPerks([])
+      let newChosenPerks = []
+      let alreadyChosen = []
+      let numberOfPerksToChooseFrom = currentAllowedPerks.length < 4 ? currentAllowedPerks.length : 4
+      for (let i = 0; i < numberOfPerksToChooseFrom ; i++) {
+        let randomNumber;
+        do {
+          randomNumber = Math.floor(Math.random() * currentAllowedPerks.length);
+        } while (alreadyChosen.includes(randomNumber))
+        newChosenPerks.push(currentAllowedPerks[randomNumber].name)
+        alreadyChosen.push(randomNumber)
+      }
+      setChosenPerks(newChosenPerks)
+    }
+
+    return (
+      <div>
+        <button onClick={handleClickEvent}>Get Perks</button>
+        <p>{chosenPerks[0] || ' - '}, {chosenPerks[1] || ' - '}, {chosenPerks[2] || ' - '}, {chosenPerks[3] || ' - '}</p>
+      </div>
+    )
+
+  }
+
+  function PerkList() {
+    const [perks, setPerks] = useRecoilState(perksState)
+
+    function handleClick(selectAll) {
+      const newPerksState = perks.map(perk => {
+        return {
+          ...perk,
+          allowed: selectAll
+        };
+      });
+
+      setPerks(newPerksState)
+    }
+
+    return(
+      <div >
+        <button onClick={() => handleClick(true)}>Select All</button>
+        <button onClick={() => handleClick(false)}>Unselect All</button>
+        {perks.map((perk) => {
+          return(
+              <Perk key={perk.id} perk={perk} />
+          ) 
+        })}
+      </div>
+    )
+  }
+
+  function TraitList() {
+    const [traits, setTraits] = useRecoilState(traitsState)
+
+    function handleClick(selectAll) {
+      const newTraitsState = traits.map(trait => {
+        return {
+          ...trait,
+          allowed: selectAll
+        };
+      });
+      setTraits(newTraitsState);
+    }
+
+    return(
+      <div >
+        <button onClick={() => handleClick(true)}>Select All</button>
+        <button onClick={() => handleClick(false)}>Unselect All</button>
+        {traits.map((trait) => {
+          return(
+              <Trait key={trait.id} trait={trait} />
+          ) 
+        })}
+      </div>
+    )
+  }
+
+  function SurvivorList() {
+    const [survivors, setSurvivors] = useRecoilState(survivorsState)
+
+    function handleClick(selectAll) {
+      const newSurvivorsState = survivors.map(survivor => {
+        return {
+          ...survivor,
+          allowed: selectAll
+        };
+      });
+      setSurvivors(newSurvivorsState);
+    }
+
+      return(
+        <div >
+          <button onClick={() => handleClick(true)}>Select All</button>
+          <button onClick={() => handleClick(false)}>Unselect All</button>
+          {survivors.map((survivor) => {
+            return(
+                <Survivor key={survivor.id} survivor={survivor} />
+            ) 
+          })}
+        </div>
+      )
+  }
+
+  function SurvivorSelector() {
+    const [chosenSurvivor, setChosenSurvivor] = useState("");
+    const allSurvivors = useRecoilValue(survivorsState);
+    const currentAllowedSurvivors = allSurvivors.filter(s => s.allowed);
+
+    function handleClick() {
+      setChosenSurvivor("");
+      let randomNumber = Math.floor(Math.random() * currentAllowedSurvivors.length);
+      setChosenSurvivor(currentAllowedSurvivors[randomNumber].name)
+    }
+
+    return(
+      <div>
+        <button onClick={handleClick}>Get Survivor</button>
+        <p>{chosenSurvivor}</p>
+      </div>
+    )
+  }
+
+  function Survivors() {
+    return(
+      <div>
+        <Container>
+          <Row>
+            <Col><SurvivorSelector/></Col>
+            <Col><BuildGenerator/></Col>
+          </Row>
+          <Row>
+            <Col><SurvivorList/></Col>
+            <Col><PerkList/></Col>
+            <Col><TraitList/></Col>
+          </Row>
+        </Container>
+      </div>
+    )
+  }
+
+  function Killers() {
+    return(
+      <div>
+        <p>WIP</p>
+      </div>
+    )
+  }
+
+  const [showSurvivorSection, setShowSurvivorSection] = useState(true);
+
+  function handleToggle(event) {
+    setShowSurvivorSection(event.target.checked);
+  }
 
   return (
     <div>
-      {perks.map((perk) => {
-        return (
-          <Perk key={perk.id} perk={perk} allowedPerks={allowedPerks} setAllowedPerks={setAllowedPerks}  />
-        ) 
-      })}
+      <RecoilRoot>
+        <Form>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <span style={{ marginRight: '0.5rem' }}>Killers</span>
+            <Form.Check
+              onChange={(event) => handleToggle(event)}
+              type="switch"
+              id="kiler-or-survivor-toggle"
+              label="Survivors"
+              checked={showSurvivorSection}
+            />
+          </div>        </Form>
+        <Collapse in={!showSurvivorSection}>
+          <div>
+            <Killers/>
+          </div>
+      </Collapse>
+        <Collapse in={showSurvivorSection}>
+          <div>
+            <Survivors/>
+          </div>
+        </Collapse>
+      </RecoilRoot>
     </div>
   )
 
