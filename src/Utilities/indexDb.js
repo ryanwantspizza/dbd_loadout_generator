a4c203fa49d5d38d1bd64b388f1a8cc1c4175837
Ryan Hubble <rhubble1987@gmail.com>
2024-01-26 09:42:17 -0500

    more changes

---
+++ src/Utilities/indexDb.js
@@ -0,0 +1,41 @@
+const dbName = "dbd_buildout_db";
+
+export const initDB = () => {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(dbName, 1);
+
+    request.onerror = (event) => {
+      reject("IndexedDB error: " + event.target.errorCode);
+    };
+
+    request.onsuccess = (event) => {
+      resolve(event.target.result);
+    };
+
+    request.onupgradeneeded = (event) => {
+      const db = event.target.result;
+      if (!db.objectStoreNames.contains('notAllowed')) {
+        db.createObjectStore('notAllowed', { keyPath: 'id' });
+      }
+      if (!db.objectStoreNames.contains('currentSelections')) {
+        db.createObjectStore('currentSelections', { keyPath: 'id' });
+      }
+    };
+  });
+};
+
+export const updateData = (db, storeName, data) => {
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction([storeName], "readwrite");
+    const store = transaction.objectStore(storeName);
+    const request = store.put(data);
+
+    request.onsuccess = () => {
+      resolve();
+    };
+
+    request.onerror = (event) => {
+      reject("Error updating data: " + event.target.errorCode);
+    };
+  });
+};
