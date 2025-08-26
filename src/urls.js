4c5949ef6a4186b010300a8cfb5dbf02dee88ed1
Ryan Hubble <ryanhubble@Mac.attlocal.net>
2025-08-26 03:01:17 -0400

    fix URLs

--- src/urls.js
+++ src/urls.js
@@ -1,11 +1,11 @@
 export const urls = {
-    survivorItems: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR4W3mqgNZMO0dVt-8zx1H4R9I1tcvo6P66Im5U-QQTSmxhECopKzGbgxvlPdZRyUZGA-tiFihhgfne/pub?gid=65561236&single=true&output=csv",
-    survivorItemAddOns: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR4W3mqgNZMO0dVt-8zx1H4R9I1tcvo6P66Im5U-QQTSmxhECopKzGbgxvlPdZRyUZGA-tiFihhgfne/pub?gid=1119670981&single=true&output=csv",
-    offerings: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR4W3mqgNZMO0dVt-8zx1H4R9I1tcvo6P66Im5U-QQTSmxhECopKzGbgxvlPdZRyUZGA-tiFihhgfne/pub?gid=1403627140&single=true&output=csv",
-    survivorPerks: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR4W3mqgNZMO0dVt-8zx1H4R9I1tcvo6P66Im5U-QQTSmxhECopKzGbgxvlPdZRyUZGA-tiFihhgfne/pub?gid=435978149&single=true&output=csv",
-    surivors: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR4W3mqgNZMO0dVt-8zx1H4R9I1tcvo6P66Im5U-QQTSmxhECopKzGbgxvlPdZRyUZGA-tiFihhgfne/pub?gid=2090851778&single=true&output=csv",
-    survivorPerkTraits: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR4W3mqgNZMO0dVt-8zx1H4R9I1tcvo6P66Im5U-QQTSmxhECopKzGbgxvlPdZRyUZGA-tiFihhgfne/pub?gid=728899707&single=true&output=csv",
-    killers: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR4W3mqgNZMO0dVt-8zx1H4R9I1tcvo6P66Im5U-QQTSmxhECopKzGbgxvlPdZRyUZGA-tiFihhgfne/pub?gid=1846234621&single=true&output=csv",
-    killerAddOns: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR4W3mqgNZMO0dVt-8zx1H4R9I1tcvo6P66Im5U-QQTSmxhECopKzGbgxvlPdZRyUZGA-tiFihhgfne/pub?gid=231795414&single=true&output=csv",
-    killerPerks: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR4W3mqgNZMO0dVt-8zx1H4R9I1tcvo6P66Im5U-QQTSmxhECopKzGbgxvlPdZRyUZGA-tiFihhgfne/pub?gid=2002176471&single=true&output=csv"
+    survivorItems: "https://docs.google.com/spreadsheets/d/1OhwAUj924Q8l6mVWRzDxEU53Gspa0Ken7urgcW2yN_Q/export?format=csv&gid=65561236",
+    survivorItemAddOns: "https://docs.google.com/spreadsheets/d/1OhwAUj924Q8l6mVWRzDxEU53Gspa0Ken7urgcW2yN_Q/export?format=csv&gid=1119670981",
+    offerings: "https://docs.google.com/spreadsheets/d/1OhwAUj924Q8l6mVWRzDxEU53Gspa0Ken7urgcW2yN_Q/export?format=csv&gid=1403627140",
+    survivorPerks: "https://docs.google.com/spreadsheets/d/1OhwAUj924Q8l6mVWRzDxEU53Gspa0Ken7urgcW2yN_Q/export?format=csv&gid=435978149",
+    surivors: "https://docs.google.com/spreadsheets/d/1OhwAUj924Q8l6mVWRzDxEU53Gspa0Ken7urgcW2yN_Q/export?format=csv&gid=2090851778",
+    survivorPerkTraits: "https://docs.google.com/spreadsheets/d/1OhwAUj924Q8l6mVWRzDxEU53Gspa0Ken7urgcW2yN_Q/export?format=csv&gid=728899707",
+    killers: "https://docs.google.com/spreadsheets/d/1OhwAUj924Q8l6mVWRzDxEU53Gspa0Ken7urgcW2yN_Q/export?format=csv&gid=1846234621",
+    killerAddOns: "https://docs.google.com/spreadsheets/d/1OhwAUj924Q8l6mVWRzDxEU53Gspa0Ken7urgcW2yN_Q/export?format=csv&gid=231795414",
+    killerPerks: "https://docs.google.com/spreadsheets/d/1OhwAUj924Q8l6mVWRzDxEU53Gspa0Ken7urgcW2yN_Q/export?format=csv&gid=2002176471"
 }
